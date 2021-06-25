0d23172db0e2714ea83a65d5fae4ae475a91d6a3
Cyrielle Le Guillous <cleguillous@sqli.com>
2021-06-25 13:53:24 +0200

    feat(turnRightAndTurnLeft) : adding the test of turn left and turn right

--- src/app/kata-rover/kata-rover.spec.ts
+++ src/app/kata-rover/kata-rover.spec.ts
@@ -1,4 +1,4 @@
-import { Direction, Position, Rover } from './kata-rover';
+import {Direction, Position, Rover} from './kata-rover';
 
 describe('Rover', () => {
     const expectRoverToBeAtPosition = (rover: Rover | undefined, expectedPosition: Position): void => {
@@ -11,13 +11,13 @@ describe('Rover', () => {
     }
 
     it('should initially be at its starting point', () => {
-        expectInitialPositionToBeTheOneOfTheRover([0, 0])([0, 0]);
-        expectInitialPositionToBeTheOneOfTheRover([1, 1])([1, 1]);
-        expectInitialPositionToBeTheOneOfTheRover([1, 2])([1, 2]);
+        expectInitialPositionToBeTheOneOfTheRover({x: 0, y: 0})({x: 0, y: 0});
+        expectInitialPositionToBeTheOneOfTheRover({x: 1, y: 1})({x: 1, y: 1});
+        expectInitialPositionToBeTheOneOfTheRover({x: 1, y: 2})({x: 1, y: 2});
     });
 
-    it('should initially face West',() => {
-        let rover = new Rover([0, 0], 'W');
+    it('should initially face West', () => {
+        let rover = new Rover({x: 0, y: 0}, 'W');
         expect(rover.direction).toStrictEqual('W');
     });
 
@@ -40,10 +40,67 @@ describe('Rover', () => {
         expectPositionAfterMoving(rover, expectedPosition);
     };
 
-    it('should move the rover forward ',() => {
-        expectRoverToMoveForward([0, 0], 'W')([-1, 0]);
-        expectRoverToMoveForward([2, 0], 'W')([1, 0]);
-        expectRoverToMoveForward([0, 3], 'W')([-1, 3]);
-        expectRoverToMoveForward([0, 0], 'E')([1, 0]);
+
+    it('should move the rover forward ', () => {
+        expectRoverToMoveForward({x: 0, y: 0}, 'W')({x: -1, y: 0});
+        expectRoverToMoveForward({x: 2, y: 0}, 'W')({x: 1, y: 0});
+        expectRoverToMoveForward({x: 0, y: 3}, 'W')({x: -1, y: 3});
+        expectRoverToMoveForward({x: 0, y: 0}, 'E')({x: 1, y: 0});
+        expectRoverToMoveForward({x: 2, y: 0}, 'E')({x: 3, y: 0});
+        expectRoverToMoveForward({x: 0, y: 3}, 'E')({x: 1, y: 3});
+        expectRoverToMoveForward({x: 0, y: 0}, 'S')({x: 0, y: -1});
+        expectRoverToMoveForward({x: 2, y: 0}, 'S')({x: 2, y: -1});
+        expectRoverToMoveForward({x: 0, y: 0}, 'N')({x: 0, y: 1});
+    });
+
+    const moveBackward = (rover: Rover) => {
+        rover.move("b");
+    };
+
+    const expectRoverToMoveBackward = (initialPosition: Position, direction: Direction) => (expectedPosition: Position) => {
+        let rover = initRoverPositionAndDirection(initialPosition, direction);
+        moveBackward(rover);
+        expectPositionAfterMoving(rover, expectedPosition);
+    };
+    it('should move the rover backward ', () => {
+        expectRoverToMoveBackward({x: 0, y: 0}, 'N')({x: 0, y: -1});
+        expectRoverToMoveBackward({x: 0, y: 0}, 'S')({x: 0, y: 1});
+        expectRoverToMoveBackward({x: 0, y: 0}, 'W')({x: 1, y: 0});
+        expectRoverToMoveBackward({x: 0, y: 0}, 'E')({x: -1, y: 0});
+    });
+
+    const turnLeft = (rover: Rover) => {
+        rover.turn('l');
+    };
+
+    const expectRoverToTurnLeft = (initialDirection: Direction) => (expectedDirection: Direction) => {
+        let rover = initRoverPositionAndDirection({x: 0, y: 0}, initialDirection);
+        turnLeft(rover);
+        expect(rover.direction).toBe(expectedDirection);
+    };
+
+    it('should turn the rover to the left', () => {
+        expectRoverToTurnLeft("N")("W");
+        expectRoverToTurnLeft("W")("S");
+        expectRoverToTurnLeft("S")("E");
+        expectRoverToTurnLeft("E")("N");
+    });
+
+    const turnRight = (rover: Rover) => {
+        rover.turn('r');
+    };
+
+    const expectRoverToTurnLRight = (initialDirection: Direction) => (expectedDirection: Direction) => {
+        let rover = initRoverPositionAndDirection({x: 0, y: 0}, initialDirection);
+        turnRight(rover);
+        expect(rover.direction).toBe(expectedDirection);
+    };
+
+
+    it('should turn the rover to the right', () => {
+        expectRoverToTurnLRight("E")("S");
+        expectRoverToTurnLRight("N")("E");
+        expectRoverToTurnLRight("W")("N");
+        expectRoverToTurnLRight("S")("W");
     });
 });
--- src/app/kata-rover/kata-rover.ts
+++ src/app/kata-rover/kata-rover.ts
@@ -3,10 +3,68 @@ export class Rover {
     }
 
     public move(command: Command) {
-        this.position = [this.position[0] - 1, this.position[1]];
+        if (command === "f") {
+            this.moveForward();
+        }
+        if (command === "b") {
+            this.moveBackward();
+        }
+    }
+
+    private moveBackward() {
+        if (this.direction === 'W') {
+            this.position.x++;
+        } else if (this.direction === 'E') {
+            this.position.x--;
+        } else if (this.direction === 'S') {
+            this.position.y++;
+        } else if (this.direction === 'N') {
+            this.position.y--;
+        }
+    }
+
+    private moveForward() {
+        if (this.direction === 'W') {
+            this.position.x--;
+        } else if (this.direction === 'E') {
+            this.position.x++;
+        } else if (this.direction === 'S') {
+            this.position.y--;
+        } else if (this.direction === 'N') {
+            this.position.y++;
+        }
+    }
+
+    turn(command: Command) {
+        if (command === "l") {
+            if (this.direction === "N") {
+                this.direction = "W";
+            } else if (this.direction === "W") {
+                this.direction = "S";
+            } else if (this.direction === "S") {
+                this.direction = "E";
+            } else if (this.direction === "E") {
+                this.direction = "N";
+            }
+        }
+        if (command === "r") {
+            if (this.direction === "N") {
+                this.direction = "E";
+            } else if (this.direction === "W") {
+                this.direction = "N";
+            } else if (this.direction === "S") {
+                this.direction = "W";
+            } else if (this.direction === "E") {
+                this.direction = "S";
+            }
+        }
     }
 }
 
-export type Position = [number, number];
+export interface Position {
+    x: number,
+    y: number
+}
+
 export type Direction = 'N' | 'S' | 'E' | 'W';
 export type Command = 'f' | 'b' | 'l' | 'r';
